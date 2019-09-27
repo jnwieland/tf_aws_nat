94a1b96d31cd7ad33e0c8f3e8f6d2cf9f0debde4
Jeff Wieland <jwieland@tree.com>
2019-09-26 22:13:44 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -38,6 +38,17 @@ data "template_file" "user_data" {
   }
 }
 
+resource "aws_eip" "nat" {
+  vpc     = true
+  count   = "${var.instance_count}"
+}
+
+resource "aws_eip_association" "eip_assoc" {
+  instance_id   = "${aws_instance.nat.id}"
+  allocation_id = "${aws_eip.nat.id}"
+  count   = "${var.instance_count}"
+}
+
 resource "aws_instance" "nat" {
   count                  = "${var.instance_count}"
   ami                    = "${data.aws_ami.ami.id}"
@@ -47,6 +58,6 @@ resource "aws_instance" "nat" {
   key_name               = "${var.aws_key_name}"
   subnet_id              = "${element(var.public_subnet_ids, count.index)}"
   vpc_security_group_ids = "${var.vpc_security_group_ids}"
-  tags                   = "${merge(var.tags, map("Name", format("%s-nat-%s", var.name, data.aws_region.current.name)))}"
+  tags                   = "${merge(var.tags, map("Name", format("%s-nat-%s", var.name, aws_instance.nat.availability_zone)))}"
   user_data              = "${element(data.template_file.user_data.*.rendered, count.index)}"
 }
